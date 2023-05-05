d917463abb8e0bec6c654b707e6cadaabe9a9bed
Ali <secure73@gmail.com>
2023-05-05 11:31:44 +0200

    first component Login

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import Login from './Login';
+function App() {
+  return (
+    <div>
+      <Login/>
+      <Login/>
+      <Login/>
+      <Login/>
+      <Login/>
+      <Login/>
+
+    </div>
+  );
+}
+
+export default App;
